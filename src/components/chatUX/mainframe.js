b13e93c4d55fb04a3897d229a1bea5bf0c7d0b27
keldath <keldath@gmail.com>
2021-03-11 22:45:51 +0200

    mount err fix
    fixed a re render issue with componentmount

--- src/components/chatUX/mainframe.js
+++ src/components/chatUX/mainframe.js
@@ -7,33 +7,55 @@ import { Component, Fragment } from 'react';
  link :
  https://stackoverflow.com/questions/53628191/edge-script1028-expected-identifier-string-or-number
 */
+
+/*without this indicator - the component gets an err
+ src: https://stackoverflow.com/questions/53949393/cant-perform-a-react-state-update-on-an-unmounted-component
+*/
+
 class MainFrame extends Component {
-
-    state = {
+   
+    /*without this indicator - the component gets an err
+    src: https://stackoverflow.com/questions/53949393/cant-perform-a-react-state-update-on-an-unmounted-component
+    */
+    constructor(props){
+		super(props);
+		this.cmpMounted = true;
+		this.state = {
         inputmsg: '',
-        publicchatlog: [
-            {   
-                time: '',
-                txt: '' ,
-                user: ''
-            }
-        ]
+            publicchatlog: [
+                {   
+                    time: '',
+                    txt: '' ,
+                    user: ''
+                }
+            ]
+        }
     }
-
     componentDidMount = () => {
+        if (!this.cmpMounted) {
+            return
+        } 
+        this.cmpMounted = true;  
         this.props.socket.on('userMsgReceived', (data) => {
-            console.log('sagi '+data)
+            console.log('sagi '+ data)
             this.updateChatState(data)
         })
     }
+    componentWillUnmount =() => {
+        this.cmpMounted = false;
+    }
 
     msgHandler = (e) => {
+        if(!this.cmpMounted)
+            return
         this.setState({
             inputmsg: e.target.value
         })
     }
 
     updateChatState = (msg) => {
+        if(!this.cmpMounted)
+            return
         const timestamp = Date.now();
         const time = new Intl.DateTimeFormat('en-US', {year: 'numeric', 
                                     day: '2-digit', month: '2-digit',
@@ -54,12 +76,13 @@ class MainFrame extends Component {
     }
 
     publicmsghandler = (e) => {
-        this.setState({...this.state,inputmsg: ''})
+        if(!this.cmpMounted)
+            return
         this.props.socket.emit('userMsgReceived', { msg: this.state.inputmsg });
+        this.setState({...this.state,inputmsg: ''})
     }
 
     chatBoxUpdate () {
-
         return this.state.publicchatlog.map((item,idx)=> {
 
             if (item.time !== '')
@@ -81,7 +104,7 @@ class MainFrame extends Component {
                     <div className='chatpublicbox'>
                         {this.chatBoxUpdate()}
                     </div>
-                    <label htmlFor='msgbox' >write here
+                    <label htmlFor='msgbox'>write here: 
                         <input type='text' name='msgbox' value={this.state.inputmsg} 
                                     onChange={this.msgHandler}/> 
                     </label>
